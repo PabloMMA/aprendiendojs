b5427f084a2f77599c35c8bd5fa85cfd0dd697a5
John DoePablo <pablo.m.meza@gmail.com>
2022-07-19 21:10:23 -0300

    primer entrega

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+/* function saludar () {
+    console.log("Hola, como estas?");
+}
+
+saludar() */
+
+function suma(a, b) {
+    let numero = a + b
+    console.log(numero);
+}
+suma(2, 6)
+
+let numeroA = parseInt(prompt("Ingresa un numero"))
+let numeroB = parseInt(prompt("Ingresa un numero"))
+suma(numeroA, numeroB)
---
+++ JS/main2.js
@@ -0,0 +1,71 @@
+let servicio1 = "Channel Manager (Para Grupos)"
+let precioServicio1 = 900
+let cantidadHoteles1 = 20
+
+let servicio2 = "Motor De Reservas (Para Grupos)"
+let precioServicio2 = 700
+let cantidadHoteles2 = 20
+
+let servicio3 = "Shopper (Para Grupos)"
+let precioServicio3 = 650
+let cantidadHoteles3 = 20
+
+
+let kitServicios = prompt("Cuantos Servicios Necesitas para tu Grupo de hoteles: \n- Channel Manager (Para Grupos)\n- Motor De Reservas (Para Grupos)\n- Shopper (Para Grupos)")
+let precioTotal = 0;
+
+function presupuesto(cantidad, precio){
+    precioTotal += cantidad * precio
+}
+
+function presupuestoGrupo(cantidad, stock, precio){
+    if(stock >= cantidad){
+        presupuesto(cantidad, precio)
+        alert("El presupuesto final es de: $" + (cantidad * precio))
+    }
+    else{
+        alert("No podemos procesar su solicitud, nuestra disponibilidad es para antender hasta: " + stock + " hoteles")
+    }
+}
+
+for(let i = 0; i < cantidadCompras; i++){
+
+    let presupuesto1 = prompt("Ingrese el nombre del producto que quiere comprar: \n- Channel Manager (Para Grupos)\n- Motor De Reservas (Para Grupos)\n- Shopper (Para Grupos)")
+    let grupoHotelero = prompt("Cuantos hoteles tiene tu grupo?:")
+
+    if(presupuesto1 == "Channel Manager (Para Grupos)"){
+        calculoStock(servicio1, precioServicio1, cantidadHoteles1)
+    }
+    else if(presupuesto1 == "Motor De Reservas (Para Grupos)"){
+        calculoStock(servicio2, precioServicio2, cantidadHoteles2)
+    }
+    else if(presupuesto1 == "Shopper (Para Grupos)"){
+        calculoStock(servicio3, precioServicio3, cantidadHoteles3)
+    }
+    else{
+        alert("Aún no tenemos ese Servicio disponible")
+    }
+}
+
+switch(true){
+    case precioTotal < 8000:
+        precioTotal = precioTotal * 0.90
+        alert("Beneficio Bronce con descuento del 10% aplicado")
+        break;
+    case precioTotal < 15000:
+        precioTotal = precioTotal * 0.80
+        alert("Beneficio Plata con descuento del 20% aplicado")
+        break;
+    case precioTotal > 15000:
+        alert("Beneficio Bronce con descuento del 30% aplicado")
+        precioTotal = precioTotal * 0.70
+        break;
+    default:
+        console.log(precioTotal)
+        alert("Intenta Nuevamente");
+        break;
+}
+
+
+
+alert("Gracias por elegirnos, uno de nuestros agentes te contactaraá pronto. Tu presupuesto hasta ahora es de: " + precioToTalConImpuestos)
