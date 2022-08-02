006d613170287be50aa6b6575b243f8ea372d316
John DoePablo <pablo.m.meza@gmail.com>
2022-08-02 16:16:09 -0300

    segunda entrega

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,7 @@
 
 saludar() */
 
-function suma(a, b) {
+/* function suma(a, b) {
     let numero = a + b
     console.log(numero);
 }
@@ -12,4 +12,20 @@ suma(2, 6)
 
 let numeroA = parseInt(prompt("Ingresa un numero"))
 let numeroB = parseInt(prompt("Ingresa un numero"))
-suma(numeroA, numeroB)
+
+suma(numeroA, numeroB) */
+
+function porCadaUno(numeros,fn) {
+   for (const el of numeros) {
+    fn(el)
+   }
+}
+
+let total = 3
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+porCadaUno (numeros, console.log)
+
+porCadaUno(numeros, (numeros) => {total =+ numeros})
+
--- JS/main2.js
+++ JS/main2.js
@@ -1,4 +1,16 @@
-let servicio1 = "Channel Manager (Para Grupos)"
+function Producto(nombre, precio, stock, categoria){
+    this.nombre = nombre
+    this.precio = precio
+    this.stock = stock
+    this.categoria = categoria
+}
+
+const servicio1 = new Producto("Channel Manager (Para Grupos)", 900, 20)
+const servicio2 = new Producto("Motor De Reservas (Para Grupos)", 700, 20)
+const servicio3 = new Producto("Shopper (Para Grupos)", 650, 20)
+
+
+/* let servicio1 = "Channel Manager (Para Grupos)"
 let precioServicio1 = 900
 let cantidadHoteles1 = 20
 
@@ -8,8 +20,9 @@ let cantidadHoteles2 = 20
 
 let servicio3 = "Shopper (Para Grupos)"
 let precioServicio3 = 650
-let cantidadHoteles3 = 20
+let cantidadHoteles3 = 20 */
 
+let listaServicioss = [servicio1, servicio2, servicio3]
 
 let kitServicios = prompt("Cuantos Servicios Necesitas para tu Grupo de hoteles: \n- Channel Manager (Para Grupos)\n- Motor De Reservas (Para Grupos)\n- Shopper (Para Grupos)")
 let precioTotal = 0;
